cc43d6a5f439bbe61225b27f64fb26de860f1c9f
jfremond <jfremond@student.42.fr>
2020-10-31 17:59:43 +0100

    Adding get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/31 17:41:12 by jfremond          #+#    #+#             */
+/*   Updated: 2020/10/31 17:43:33 by jfremond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*ft_strjoin(const char *s1, const char *s2);
+
+size_t	ft_strlen(const char *str);
+
+int		ft_newline(char *str);
+int		get_next_line(int fd, char **line);
+
+#endif
