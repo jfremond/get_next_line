923baa31234f980d422335b1cd467b199433ba44
jfremond <jfremond@student.42.fr>
2020-11-02 14:45:22 +0100

    Adding get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 17:41:12 by jfremond          #+#    #+#             */
-/*   Updated: 2020/10/31 17:43:33 by jfremond         ###   ########.fr       */
+/*   Updated: 2020/11/02 13:43:08 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 char	*ft_strjoin(const char *s1, const char *s2);
 
 size_t	ft_strlen(const char *str);
+
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 int		ft_newline(char *str);
 int		get_next_line(int fd, char **line);
