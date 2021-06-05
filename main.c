86c9469dd80d0cae08adab0d07234bcab3fe2693
jfremond <jfremond@student.42.fr>
2021-06-05 16:04:34 +0200

    Normed :white_check_mark:

--- main.c
+++ main.c
@@ -1,22 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/05 16:02:02 by jfremond          #+#    #+#             */
+/*   Updated: 2021/06/05 16:03:11 by jfremond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include "get_next_line.h"
 
-int		main(int argc, char const **argv)
+int	main(int argc, char const **argv)
 {
-	int		test;
 	int		fd;
 	char	*line;
 	int		countline;
-	test = 0;
+
 	countline = 1;
 	line = NULL;
 	if (argc == 1)
 		fd = 0;
 	else
 		fd = open(argv[1], O_RDONLY);
-	while ((test = get_next_line(fd, &line)) > 0)
+	while (get_next_line(fd, &line) > 0)
 	{
 		printf("line %d : %s\n", countline, line);
 		countline++;
